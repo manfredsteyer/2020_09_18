c08e278dd76d3f310eaa7d1755af4be2dbe3651c
Manfred Steyer <manfred.steyer@gmx.net>
2020-09-16 14:52:32 +0200

    add luggage domain

---
+++ libs/logger-lib/src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of logger-lib
+ * Also: public_api.ts, public-api.ts
+ */
+export * from './lib/logger.config';
+export * from './lib/logger.service';
+export * from './lib/log-monitor.component';
+export * from './lib/logger.module';
